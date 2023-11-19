205d3750bf0099db648e3a5609280dbdec442f2b
Protocol <72527796+yohan900000@users.noreply.github.com>
2023-11-19 19:08:25 +0100

    Mise à jour de la migration pour le modèle "products" et ajout d'une hauteur maximale aux images dans le CSS admin.

--- database/migrations/2023_11_13_102517_create_products_table.php
+++ database/migrations/2023_11_13_102517_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->longText('description');
             $table->string('picture');
             $table->foreignId('gamme_id')->constrained('gammes')->cascadeOnDelete();
             $table->timestamps();
